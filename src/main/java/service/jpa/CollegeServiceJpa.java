a514eb636c495ea6b8be41d38de1696ed7dac928
Ji Liu <liujiboy@163.com>
2020-04-09 19:48:52 +0800

    spring-log4j

--- src/main/java/service/jpa/CollegeServiceJpa.java
+++ src/main/java/service/jpa/CollegeServiceJpa.java
@@ -2,6 +2,8 @@ package service.jpa;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,14 +13,17 @@ import repository.CollegeRepository;
 import service.CollegeService;
 @Service
 public class CollegeServiceJpa implements CollegeService{
+	private final Logger logger = LoggerFactory.getLogger(CollegeServiceJpa.class);
 	@Autowired
 	private CollegeRepository  collegeRepository;
-	
+
 	@Transactional
 	public void addColleges()
 	{
 		collegeRepository.save(new College("1","计算机学院"));
+		logger.debug("插入计算机学院成功");
 		collegeRepository.save(new College("2","通信学院"));
+		logger.debug("插入通信学院成功");
 	}
 	@Transactional
 	public void deleteAllColleges()
--- src/main/java/service/jpa/StudentServiceJpa.java
+++ src/main/java/service/jpa/StudentServiceJpa.java
@@ -3,6 +3,8 @@ package service.jpa;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,12 +16,15 @@ import repository.StudentRepository;
 import service.StudentService;
 @Service
 public class StudentServiceJpa implements StudentService {
+	private final Logger logger = LoggerFactory.getLogger(StudentServiceJpa.class);
+
 	@Autowired
 	private CollegeRepository  collegeRepository;
 	@Autowired
 	private StudentRepository studentRepository;
 	@Transactional
 	public void addStudent(String id, String name, int age, String collegeId) {
+		logger.debug("id=%s,name=%s,age=%d,collegeId=%s",id,name,age,collegeId);
 		Optional<College> college = collegeRepository.findById(collegeId);
 		if(!college.isPresent())
 			throw new RuntimeException("collegeId="+collegeId+"的college不存在!");
