f625a91ee193c76fca838d28684e36fdc55b763e
Ji Liu <liujiboy@163.com>
2020-04-11 17:23:43 +0800

    修正spring-jpa中的bug

--- src/main/java/TestPersistenceContext.java
+++ src/main/java/TestPersistenceContext.java
@@ -3,7 +3,7 @@ import javax.persistence.PersistenceContext;
 public class TestPersistenceContext {
 
 	public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, SecurityException {
-		Class clz=Class.forName("dao.jpa.CollegeRepositoryJpa");
+		Class clz=Class.forName("repository.jpa.CollegeRepositoryJpa");
 		PersistenceContext pc=clz.getDeclaredField("em").getAnnotation(PersistenceContext.class);
 		System.out.println(pc.synchronization());
 
