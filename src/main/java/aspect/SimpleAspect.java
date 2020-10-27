70e503f0a3d7873863f5f1df3b2f6c5d531d4ec4
Ji Liu <liujiboy@163.com>
2020-10-27 13:39:25 +0800

    修改程序bug

--- src/main/java/aspect/SimpleAspect.java
+++ src/main/java/aspect/SimpleAspect.java
@@ -8,22 +8,22 @@ import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class SimpleAspect {
-	@Before("execution (** bean.HelloWorld.sayHello(..) ) ")
+	@Before("execution (* bean.HelloWorld.sayHello(..) ) ")
 	public void beforeSayHello()
 	{
 		System.out.println("before say hello");
 	}
-	@After("execution (** bean.HelloWorld.sayHello(..) ) ")
+	@After("execution (* bean.HelloWorld.sayHello(..) ) ")
 	public void afterSayHello()
 	{
 		System.out.println("after say hello");
 	}
-	@AfterReturning("execution (** bean.Person.get*(..) ) ")
+	@AfterReturning("execution (* bean.Person.get*(..) ) ")
 	public void afterReturning()
 	{
 		System.out.println("afterReturning get*");
 	}
-	@AfterThrowing("execution (** bean.Person.*(..) ) ")
+	@AfterThrowing("execution (* bean.Person.*(..) ) ")
 	public void afterThrowing()
 	{
 		System.out.println("afterThrowing throw a exception");
